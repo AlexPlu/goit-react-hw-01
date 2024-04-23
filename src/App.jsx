92705e9d91ea23494df14cb5ec5104d949472e9c
Oleksandr Pluzhnykov <50083590+AlexPlu@users.noreply.github.com>
2024-04-24 02:11:23 +0300

    hw-01

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
 import userData from "./components/Profile/userData.json";
+import friends from "./components/FriendList/friends.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./components/TransactionHistory/transactions.json";
 
 const App = () => {
   return (
-    <Profile
-      name={userData.name}
-      tag={userData.tag}
-      location={userData.location}
-      image={userData.avatar}
-      stats={userData.stats}
-    />
+    <>
+      <Profile
+        name={userData.name}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
 
